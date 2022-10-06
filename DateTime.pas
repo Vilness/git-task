f5f4c75aae6b594da34527f429f8e13f12cdcfe3
Chubenko Ivan <ivan_0572@list.ru>
2022-10-06 11:33:54 +0300

    Revert "Add convert from minutes to seconds"
    This reverts commit f3a2e4ef9d2d2c9176157618df4f7bb7c840a74a.

--- DateTime.pas
+++ DateTime.pas
@@ -6,7 +6,3 @@ var y := ReadInteger('Введите текущий год:');
 Assert(y > 0, 'Год должен быть положителен!');
 
 Println(y.Divs(4) and (y.Divs(400) or not(y.Divs(100))) ? True : False);
-
-// Программа №2
-
-Println('Кол-во секунд в минуте: 60');
