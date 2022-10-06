b646055463aed7b254aabdfdfad86ce6e4e8b2f2
Chubenko Ivan <ivan_0572@list.ru>
2022-10-06 11:28:56 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,8 @@
+﻿##
+
+// Программа №1
+
+var y := ReadInteger('Введите текущий год:');
+Assert(y > 0, 'Год должен быть положителен!');
+
+Println(y.Divs(4) and (y.Divs(400) or not(y.Divs(100))) ? True : False);
